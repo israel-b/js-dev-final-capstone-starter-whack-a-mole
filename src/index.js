583e044828408cb437d28661bc3ea43bea88145f
israel-b <briggs.israel@gmail.com>
2023-11-15 10:54:22 -0800

    Added comments, fixed bg image

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('#start');
-// TODO: Add the missing query selectors:
 const score = document.querySelector('#score'); // Use querySelector() to get the score element
 const timerDisplay = document.querySelector('#timer'); // use querySelector() to get the timer element.
 
@@ -185,7 +184,6 @@ function clearScore() {
 *
 */
 function updateTimer() {
-
   if (time > 0){
     time -= 1; 
     timerDisplay.textContent = time;
